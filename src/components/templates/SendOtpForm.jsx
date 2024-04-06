08e58b2d9504482fade7c26d32065b451be181c4
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-04-06 13:27:35 +0330

    Feat:create sendotp form and send data to backend

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { sendOtp } from '../../services/auth'
 
-function SendOtpForm() {
+function SendOtpForm({setStep , setMobile , mobile}) {
+
+  const submitHandler = async (event) => {
+    event.preventDefault()
+    if(mobile.length !== 11) return
+
+    const {response, error} = await sendOtp(mobile)
+    if(response) setStep(2)
+    if(error) console.log(error)
+  }
   return (
-    <div>SendOtpForm</div>
+    <form onSubmit={submitHandler}>
+      <p>ورود به حساب کاربری</p>
+      <span>
+        برای استفاده از امکانات دیوار, لطفا شماره موبایل خود را وارد کنید. کد تایید به این شماره پیامک خواهد شد.
+      </span>
+      <label htmlFor='input'>شماره موبایل خود را وارد کنید.</label>
+      <input type="text" placeholder='شماره موبایل' id='input' value={mobile} onChange={e => setMobile(e.target.value)}/>
+      <button type='submit'>تایید</button>
+    </form>
   )
 }
 
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -9,7 +9,7 @@ function AuthPage() {
     const [code,setCode] = useState("")
   return (
     <div> 
-        {step === 1 && <SendOtpForm/>}
+        {step === 1 && <SendOtpForm setStep={setStep} mobile={mobile} setMobile={setMobile}/>}
         {step === 2 && <CheckOtpForm/>}
     </div>
   )
