56cb5373f54fd8789e6bb31f6adb970c25a53f35
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-05-03 19:32:12 +0330

    Feat & Styles:send data of category

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import {useMutation} from '@tanstack/react-query'
+import { addCategory } from '../../services/admin'
 //styles
 import styles from "./CategoryForm.module.css"
 
@@ -8,22 +10,28 @@ function CategoryForm() {
         slug:"",
         icon:"",
     })
+    const {mutate,isLoading,error,data} = useMutation(addCategory)
+
     const changeHandler = (event) => {
         setForm({...form,[event.target.name]:event.target.value})
     }
     const submitHandler = (event) => {
         event.preventDefault()
+        if(!form.name || !form.slug || !form.icon) return
+        mutate(form)
     }
   return (
     <form onChange={changeHandler} onSubmit={submitHandler} className={styles.form}>
         <h3>دسته بندی جدید</h3>
+        {!!error && <p>مشکلی پیش امده است.</p>}
+        {data?.status === 201 && <p>دسته بندی با موفقیت اضافه شد.</p>}
         <label htmlFor='name'>اسم دسته بندی</label>
         <input type='text' name='name' id='name'/>
         <label htmlFor='slug'>اسلاگ</label>
         <input type='text' name='slug' id='slug'/>
         <label htmlFor='icon'>ایکون</label>
         <input type='text' name='icon' id='icon'/>
-        <button type='submit'>ایجاد</button>
+        <button type='submit' disabled={isLoading}>ایجاد</button>
     </form>
   )
 }
