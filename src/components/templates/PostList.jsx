5ffa8ed9fa146b099a78dcdb669da3b9f07b09e6
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-05-09 20:03:16 +0330

    Style:style of postList component

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -5,23 +5,25 @@ import { getPosts } from '../../services/user'
 import { sp } from '../../utils/number'
 //components
 import Loader from "../modules/Loader"
+//styles
+import styles from "./PostList.module.css"
 
 function PostList() {
     const baseURL = import.meta.env.VITE_BASE_URL
     const [data,isLoading] = useQuery("my-post-list",getPosts)
   return (
-    <div>
+    <div className={styles.list}>
       {isLoading ? <Loader/> : (
         <>
           <h3>اگهی های شما</h3>
           {data.data.posts.map(post => (
-            <div key={post._id}>
+            <div key={post._id} className={styles.post}>
               <img src={`${baseURL}${post.images[0]}`} />
               <div>
                 <p>{post.options.title}</p>
                 <span>{post.options.content}</span>
               </div>
-              <div>
+              <div className={styles.price}>
                 <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
                 <span>{sp(post.amount)}تومان</span>
               </div>
