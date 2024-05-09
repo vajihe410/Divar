cf77bb58d2f9f125b4c7b0ee64531cdbd3728a31
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-05-09 20:23:44 +0330

    Feat:create sidebar component

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { getCategory } from '../../services/admin'
 
 function Sidebar() {
+  const {data} = useQuery(["get_categories"],getCategory)
   return (
-    <div>Sidebar</div>
+    <div>
+      <ul>
+        {data?.data.map(category => (
+          <li key={category._id}>
+            <img src={`${category.icon}.svg`} />
+            <p>{category.name}</p>
+          </li>
+        ) )}
+      </ul>
+    </div>
   )
 }
 
