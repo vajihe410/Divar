0dee2880ee18bbc1ae535a0e97cbbd9a0b085b3a
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-05-10 12:16:16 +0330

    Feat:show all posts in main components

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { sp } from '../../utils/number'
 
 function Main({posts}) {
+  const baseURL = import.meta.env.VITE_BASE_URL
   return (
-    <div>Main</div>
+    <div>
+     {posts.data.posts.map(post => (
+      <div key={post._id}>
+        <div>
+          <p>{post.options.title}</p>
+          <div>
+            <p>{sp(post.amount)} تومان</p>
+            <span>{post.options.city}</span>
+          </div>
+        </div>
+        <img src={`${baseURL}${post.images[0]}`}/>
+      </div>
+     ))}
+    </div>
   )
 }
 
